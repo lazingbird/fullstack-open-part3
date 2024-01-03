39d434dc8e6baf35be203d78b6cfac56ea58de9d
lazinbird <lazingbird@protonmail.com>
2024-01-03 15:51:01 -0300

    prepare to deploy on render.com

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("data", (req, res) => {
   return JSON.stringify(req.body);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
@@ -99,7 +101,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
