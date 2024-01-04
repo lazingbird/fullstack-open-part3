5773b84b1737b044f65bcedc0ff855d1307fbee5
lazinbird <lazingbird@protonmail.com>
2024-01-04 00:20:29 -0300

    uibuild

--- models/person.js
+++ models/person.js
@@ -26,7 +26,7 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function (v) {
-        return /\d{2,3}-/.test(v);
+        return /\d{2,3}-\d{*}/.test(v);
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
