8b492fe0ead6d535e8667d7cc934f22fee6d9bd7
lazinbird <lazingbird@protonmail.com>
2024-01-03 22:09:46 -0300

    uibuild

--- index.js
+++ index.js
@@ -18,32 +18,6 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const info = `Phonebook has info for ${persons.length} persons`;
-const date = new Date();
-
 app.get("/info", (request, response) => {
   response.send(`<p>${info}</p><p>${date}</p>`);
 });
